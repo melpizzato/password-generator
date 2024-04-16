865fcbe2341da326ebfefbbd821873ce9164ae00
Melissa Pizzato <melpizzato@hotmail.com>
2024-04-16 09:43:34 -0300

    adding input for setting password size

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 function App() {
   const [password, setPassword] = useState("")
   const [copyText, setCopyText] = useState("Copy")
+  const [passwordSize, setPasswordSize] = useState(12)
 
   function copyToClipboard() {
     setCopyText("Copied!")
@@ -12,9 +13,8 @@ function App() {
   function generate(){
     setCopyText("Copy")
     const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
-    const length = 16
     let newPassword = ""
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const position = Math.floor(Math.random() * characters.length)
       newPassword += characters[position]
     }
@@ -24,6 +24,16 @@ function App() {
   return (
     <>
       <h1>Password generator</h1>
+      <div>
+        <label htmlFor="passwordSize">Password size:</label>
+        <input 
+        type="number" 
+        id="passwordSize" 
+        min={1}
+        value={passwordSize}
+        onChange={(ev) => setPasswordSize(ev.target.value)}
+        />
+      </div>
       <div>
         <button
         onClick={generate}
